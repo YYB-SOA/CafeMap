c56b68e16fbe58b7fa032b7f9b803218b84a3665
Leo Shr <gogoloso123@gmail.com>
2022-10-30 15:36:05 +0800

    UPDATE: cafe_api.rb in gateway: Safty set

--- lib/gateways/place_api.rb
+++ lib/gateways/place_api.rb
@@ -7,11 +7,12 @@ module CafeMap
   module Place
     # Library for Place API
     class PlaceApi
-      def initialize(token_name)
-        @token_name = token_name # @place_token ＝'Place_api'
+      def initialize(token_name, store_namearr)
+        @token_name = token_name
+        @store_namearr = store_namearr
       end
 
-      def store(token_name = @token_name)
+      def store(token_name = @token_name, _name_arr = @store_namearr)
         Request.new(token_name).request_main # Array
       end
 
@@ -34,19 +35,16 @@ module CafeMap
           end
         end
 
-        def noise_filter(name_str)
-          # Regularization
-          name_str.gsub('()', '').gsub(' ', '').gsub("\b", '')
-        end
-
         def data_clean(box)
           # Input: string array of cafe name
-          box.map { |name_str| noise_filter(name_str) }
+          box.map { |name_str| name_str.gsub('()', '').gsub(' ', '').gsub("\b", '') }
         end
 
-        def request_main(name_of_key = @token_name, name_array = ["WHO'S 喜象 CAFE", 'ARTROOM14藝室'])
-          cafe_clean_name = data_clean(name_array)
-          cafe_clean_name.map do |eachstore|
+        def request_main(name_of_key = @token_name, name_array = @store_namearr, safty: true)
+          # Caution: We set "safty" for avoid overcharging from placeAPI: do not chage it for testing
+          name_array = %w[陶匠咖啡 過日子咖啡館] if safty == true
+
+          data_clean(name_array).map do |eachstore|
             call_placeapi_url(eachstore, get_placeapi_token(name_of_key)).parse
           end
         end
