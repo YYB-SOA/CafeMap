8f4c52ba2978db1e7a02641ca8275e69cfc1d998
Leo Shr <gogoloso123@gmail.com>
2022-10-30 15:17:02 +0800

    ADD: cafe_api spec data mapper branch(complete)

--- spec/cafe_api_spec.rb
+++ spec/cafe_api_spec.rb
@@ -21,97 +21,86 @@ describe 'Tests Cafe Nomad API library' do
     VCR.eject_cassette
   end
 
-  describe 'Cafe Nomad Store Status' do
-    it 'HAPPY: should provide correct api status attributes' do
-      api_status = CafeNomad::CafeApi.new('CAFE_NOMAD', CAFE_TOKEN).api_status
-      _(api_status.status).must_equal CORRECT['status']
-      _(api_status.amount).must_equal CORRECT['amount']
-      _(api_status.header).must_equal CORRECT['header']
-    end
-  end
-
   describe 'Cafe Nomad Store Content' do
     before do
-      @api_info = CafeNomad::CafeApi.new('CAFE_NOMAD', CAFE_TOKEN).api_info
+      @cafe_info = CafeMap::CafeNomad:: InfoMapper.new('Cafe_api').load_several
       @yaml_keys = CORRECT.keys[3..]
     end
 
     #--------------------------Store Content-----------
     it 'HAPPY: should pass corect id' do
-      # ans_sheet func is to replace the original duplicate code below:
-      # _(@api_info.map(&:id)).must_equal @yaml_keys.map { |item| CORRECT[item]['id'] }
-      _(@api_info.map(&:id)).must_equal ans_sheet('id', @yaml_keys)
+      _(@cafe_info.map(&:id)).must_equal ans_sheet('id', @yaml_keys)
     end
 
     it 'HAPPY: should pass corect name' do
-      _(@api_info.map(&:name)).must_equal ans_sheet('name', @yaml_keys)
+      _(@cafe_info.map(&:name)).must_equal ans_sheet('name', @yaml_keys)
 
     end
 
     it 'HAPPY: should pass corect city' do
-      _(@api_info.map(&:city)).must_equal ans_sheet('city', @yaml_keys)
+      _(@cafe_info.map(&:city)).must_equal ans_sheet('city', @yaml_keys)
     end
 
     it 'HAPPY: should pass corect url' do
-      _(@api_info.map(&:url)).must_equal ans_sheet('url', @yaml_keys)
+      _(@cafe_info.map(&:url)).must_equal ans_sheet('url', @yaml_keys)
     end
 
     it 'HAPPY: should pass corect address' do
-      _(@api_info.map(&:address)).must_equal ans_sheet('address', @yaml_keys)
+      _(@cafe_info.map(&:address)).must_equal ans_sheet('address', @yaml_keys)
     end
 
     it 'HAPPY: should pass corect latitude' do
-      _(@api_info.map(&:latitude)).must_equal ans_sheet('latitude', @yaml_keys)
+      _(@cafe_info.map(&:latitude)).must_equal ans_sheet('latitude', @yaml_keys)
     end
 
     it 'HAPPY: should pass corect longitude' do
-      _(@api_info.map(&:longitude)).must_equal ans_sheet('longitude', @yaml_keys)
+      _(@cafe_info.map(&:longitude)).must_equal ans_sheet('longitude', @yaml_keys)
     end
 
     it 'HAPPY: should pass corect limited_time' do
-      _(@api_info.map(&:limited_time)).must_equal ans_sheet('limited_time', @yaml_keys)
+      _(@cafe_info.map(&:limited_time)).must_equal ans_sheet('limited_time', @yaml_keys)
     end
 
     it 'HAPPY: should pass corrected  mrt' do
-      _(@api_info.map(&:mrt)).must_equal ans_sheet('mrt', @yaml_keys)
+      _(@cafe_info.map(&:mrt)).must_equal ans_sheet('mrt', @yaml_keys)
     end
 
     it 'HAPPY: should pass corrected  open_time' do
-      _(@api_info.map(&:open_time)).must_equal ans_sheet('open_time', @yaml_keys)
+      _(@cafe_info.map(&:open_time)).must_equal ans_sheet('open_time', @yaml_keys)
     end
 
     it 'HAPPY: should pass identitied standing_desk' do
-      _(@api_info.map(&:standing_desk)).must_equal ans_sheet('standing_desk', @yaml_keys)
+      _(@cafe_info.map(&:standing_desk)).must_equal ans_sheet('standing_desk', @yaml_keys)
     end
 
     it 'HAPPY: should pass identitied socket' do
-      _(@api_info.map(&:socket)).must_equal ans_sheet('socket', @yaml_keys)
+      _(@cafe_info.map(&:socket)).must_equal ans_sheet('socket', @yaml_keys)
     end
 
     #--------------------------Rating Content---------------------------
 
     it 'HAPPY: should pass identitied wifi score' do
-      _(@api_info.map(&:wifi)).must_equal ans_sheet('wifi', @yaml_keys)
+      _(@cafe_info.map(&:wifi)).must_equal ans_sheet('wifi', @yaml_keys).map(& :to_s)
     end
 
     it 'HAPPY: should pass identitied seat score' do
-      _(@api_info.map(&:seat)).must_equal ans_sheet('seat', @yaml_keys)
+      _(@cafe_info.map(&:seat)).must_equal ans_sheet('seat', @yaml_keys).map(& :to_s)
     end
 
     it 'HAPPY: should pass identitied quiet score' do
-      _(@api_info.map(&:quiet)).must_equal ans_sheet('quiet', @yaml_keys)
+      _(@cafe_info.map(&:quiet)).must_equal ans_sheet('quiet', @yaml_keys).map(& :to_s)
     end
 
     it 'HAPPY: should pass identitied tasty score' do
-      _(@api_info.map(&:tasty)).must_equal ans_sheet('tasty', @yaml_keys)
+      _(@cafe_info.map(&:tasty)).must_equal ans_sheet('tasty', @yaml_keys).map(& :to_s)
     end
 
     it 'HAPPY: should pass identitied cheap score' do
-      _(@api_info.map(&:cheap)).must_equal ans_sheet('cheap', @yaml_keys)
+      _(@cafe_info.map(&:cheap)).must_equal ans_sheet('cheap', @yaml_keys).map(& :to_s)
     end
 
     it 'HAPPY: should pass identitied music score' do
-      _(@api_info.map(&:music)).must_equal ans_sheet('music', @yaml_keys)
+      _(@cafe_info.map(&:music)).must_equal ans_sheet('music', @yaml_keys).map(& :to_s)
     end
   end
 end
--- spec/deleted.rb
+++ spec/deleted.rb
@@ -1,5 +1,30 @@
-require_relative 'place_api_spec_helper'
+require_relative 'cafe_api_spec_helper'
+
+@yaml_keys = CORRECT.keys[3..]
+# gg = PlaceInfo::PlaceApi.new(KEYWORD_FILTER, TOKEN_NAME).store(KEYWORD_FILTER,TOKEN_NAME)
+puts ans_sheet('id', @yaml_keys).class
+
+entity = CafeMap::CafeNomad:: InfoMapper.new('Cafe_api').load_several
+
+# # entity.select {|store| puts store.id}
+# puts "id NotPassed " if entity.map(&:id) != ans_sheet('id', @yaml_keys)
+# puts "name NotPassed " if entity.map(&:name) != ans_sheet('name', @yaml_keys)
+# puts "city NotPassed " if entity.map(&:city) != ans_sheet('city', @yaml_keys)
+# puts "url NotPassed " if entity.map(&:url) != ans_sheet('url', @yaml_keys)
+# puts "address NotPassed " if entity.map(&:address) != ans_sheet('address', @yaml_keys)
+# puts "latitude NotPassed " if entity.map(&:latitude) != ans_sheet('latitude', @yaml_keys)
+# puts "longitude NotPassed " if entity.map(&:longitude) != ans_sheet('longitude', @yaml_keys)
+# puts "limited_time NotPassed " if entity.map(&:limited_time) != ans_sheet('limited_time', @yaml_keys)
+# puts "mrt NotPassed " if entity.map(&:mrt) != ans_sheet('mrt', @yaml_keys) 
 
 
-gg = PlaceInfo::PlaceApi.new(KEYWORD_FILTER, TOKEN_NAME).store(KEYWORD_FILTER,TOKEN_NAME)
-gg
+# ##########################
+puts "music NotPassed " if entity.map(&:music) != ans_sheet('music', @yaml_keys).map(& :to_s)
+puts "cheap NotPassed " if entity.map(&:cheap) != ans_sheet('cheap', @yaml_keys).map(& :to_s)
+puts "tasty NotPassed " if entity.map(&:tasty) != ans_sheet('tasty', @yaml_keys).map(& :to_s)
+puts "quiet NotPassed " if entity.map(&:quiet) != ans_sheet('quiet', @yaml_keys).map(& :to_s)
+puts "seat NotPassed " if entity.map(&:seat) != ans_sheet('seat', @yaml_keys).map(& :to_s)
+puts "wifi NotPassed " if entity.map(&:wifi) != ans_sheet('wifi', @yaml_keys).map(& :to_s)
+
+# p ans_sheet('music', @yaml_keys).calss
+# p entity.map(&:music)
