d4aa0884a0e533c20e573f2a38f9b68b741722db
Leo Shr <gogoloso123@gmail.com>
2022-10-30 15:59:57 +0800

    Update the entity/gateway/mappers already, hav not rewrite place_api_spec

--- spec/place_api_spec.rb
+++ spec/place_api_spec.rb
@@ -23,16 +23,21 @@ describe 'Tests Place API library' do
   describe 'Store information' do
 
     before do
-      @store = PlaceInfo::PlaceApi.new(KEYWORD_FILTER, TOKEN_NAME).store(KEYWORD_FILTER,TOKEN_NAME)
-      @array_hash = CORRECT.keys[0..].each{|key| CORRECT[key]['results']}
+      test_store = ["陶匠咖啡", "WHO'S喜象CAFE", '過日子咖啡館']
+      @store = CafeMap::Place::StoreMapper.new(TOKEN_NAME, test_store).load_several
+      @yaml_keys = CORRECT.keys[0..].each{|key| CORRECT[key]['results']}
+
+      @cafe_info = CafeMap::CafeNomad:: InfoMapper.new('Cafe_api').load_several
+      @yaml_keys = CORRECT.keys[3..]
     end
+# @yaml_keys.map { |item| CORRECT[item]['id'] }
 
     it 'HAPPY: should provide correct Store attributes' do
-
-      _( @store.place_id.must_equal @array_hash.each{|item|item.each{|i| i['place_id']} })
-      _( @store.business_status.must_equal @array_hash.map{|item|item.map{|i| i['business_status']} }[0][0])
-      _( @store.location_lat.must_equal @array_hash.map{|item|item.map{|i| i['geometry']['location']['lat']} }[0][0])
-      _( @store.location_lng.must_equal @array_hash.map{|item|item.map{|i| i['geometry']['location']['lng']} }[0][0])
+      _( @store.place_id.must_equal ans_sheet('place_id', @yaml_keys)
+      # _( @store.place_id.must_equal @yaml_keys.each{|item|item.each{|i| i['place_id']} })
+      _( @store.business_status.must_equal @yaml_keys.map{|item|item.map{|i| i['business_status']} }[0][0])
+      _( @store.location_lat.must_equal @yaml_keys.map{|item|item.map{|i| i['geometry']['location']['lat']} }[0][0])
+      _( @store.location_lng.must_equal @yaml_keys.map{|item|item.map{|i| i['geometry']['location']['lng']} }[0][0])
 
     end
   end
